fcd985d990ecfe3ba28da3bdbf5f99c5f722856d
lanternavermelha <f_figueira@hotmail.com>
2020-01-24 18:45:01 +0000

    Project created with Spring Initializer

---
+++ src/test/java/lanterna/vermelha/cruddemo/CruddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package lanterna.vermelha.cruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
