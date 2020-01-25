8e6b93776ffedb6cd940755b8178ec0d7926389a
lanternavermelha <f_figueira@hotmail.com>
2020-01-25 11:50:25 +0000

    creating CRUD methods for the DAO

--- src/main/java/lanterna/vermelha/cruddemo/dao/EmployeeDAOHibernateImpl.java
+++ src/main/java/lanterna/vermelha/cruddemo/dao/EmployeeDAOHibernateImpl.java
@@ -16,31 +16,68 @@ import lanterna.vermelha.cruddemo.entity.Employee;
 public class EmployeeDaoHibernateImpl implements EmployeeDao {
 
 	//define field for entitymanager
-	
+
 	private EntityManager entityManager;
-	
+
 	//set up constructor injection
 	@Autowired
 	public EmployeeDaoHibernateImpl(EntityManager theEntityManager) {
 		entityManager = theEntityManager;
 	}
-	
+
 	@Override
-	@Transactional
 	public List<Employee> findAll() {
-		
+
 		//get the current hibernate session
 		Session currentSession = entityManager.unwrap(Session.class);
-		
+
 		//create a query
 		Query<Employee> theQuery =
 				currentSession.createQuery("from Employee",Employee.class);
-		
+
 		//execute query and get the result list
 		List<Employee> employees = theQuery.getResultList();
-		
+
 		//return the results
 		return employees;
+	}
+
+	@Override
+	public Employee findById(int theId) {
+		// get the current hibernate session
+		Session currentSession = entityManager.unwrap(Session.class);
+
+		// get the employee
+		Employee theEmployee =
+				currentSession.get(Employee.class, theId);
+
+		//return the employee
+		return theEmployee;
+	}
+
+	@Override
+	public void save(Employee theEmployee) {
+
+		// get the current hibernate session
+		Session currentSession = entityManager.unwrap(Session.class);
+
+		//save employee
+		currentSession.saveOrUpdate(theEmployee);
+	}
+
+	@Override
+	public void deleteById(int theId) {
+
+		// get the current hibernate session
+		Session currentSession = entityManager.unwrap(Session.class);
+		
+		//delete the object with primary key
+		Query theQuery =
+				currentSession.createQuery(
+						"delete from Employee where id=:employeeId");
+		theQuery.setParameter("employeeId", theId);
+		
+		theQuery.executeUpdate();
 	}
 
 }
--- src/main/java/lanterna/vermelha/cruddemo/dao/EmployeeDao.java
+++ src/main/java/lanterna/vermelha/cruddemo/dao/EmployeeDao.java
@@ -7,5 +7,8 @@ import lanterna.vermelha.cruddemo.entity.Employee;
 public interface EmployeeDao {
 	
 	public List<Employee> findAll();
+	public Employee findById(int theId);
+	public void save(Employee theEmployee);
+	public void deleteById(int theId);
 
 }
